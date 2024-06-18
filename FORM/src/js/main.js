02608705b4753633387669647f2cb17f65e461f8
nagesh-merva <officialnagesh.merva@gmail.com>
2024-06-18 18:26:50 +0530

    first commit

---
+++ FORM/src/js/main.js
@@ -0,0 +1,55 @@
+let Discount = 10
+
+const HAT = document.getElementById('Chefshat')
+const DISCOUNT = document.getElementById('discount')
+
+document.getElementById('discountForm').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const name = document.getElementById('name').value
+    const phone = document.getElementById('phone').value
+
+    const formData = {
+        name: name,
+        phone: phone
+    };
+
+    fetch('http://127.0.0.1:5000/api/save_form_data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(response => response.text())
+        .then(data => {
+            alert('Form submitted successfully')
+            fetchWeightedValue()
+        })
+        .catch((error) => {
+            console.error('Error:', error)
+        })
+});
+
+function fetchWeightedValue() {
+    fetch('http://127.0.0.1:5000/api/get_discount_value')
+        .then(response => response.json())
+        .then(data => {
+            Discount = data.value
+            console.log(Discount)
+            updateElements()
+        })
+        .catch((error) => {
+            console.error('Error fetching weighted value:', error)
+        });
+}
+
+function updateElements() {
+    HAT.classList.remove('hidden')
+    HAT.classList.add('animate-translate-scale')
+    setTimeout(() => {
+        DISCOUNT.innerText += Discount + "% Discount"
+        DISCOUNT.classList.add('animate-scale')
+        DISCOUNT.classList.remove('hidden')
+    }, 1000)
+}
---
+++ FORM_BACKEND/main.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from datetime import datetime
+from flask_cors import CORS
+import os
+import random
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'a_secure_default_key')
+
+CORS(app, supports_credentials=True, allow_headers="*", origins="*", methods=["OPTIONS", "POST"])
+
+client = MongoClient(
+    'mongodb+srv://nagesh:nagesh2245@mywebsites.btvk61i.mongodb.net/',
+    connectTimeoutMS=30000, 
+    socketTimeoutMS=None)
+db = client['Crob_orders']
+orderslist = db['orders']
+
+@app.route('/api/save_form_data', methods=['POST', 'OPTIONS'])
+def save_form_data():
+    if request.method == 'OPTIONS':
+        return jsonify({'status': 'success', 'message': 'CORS preflight request handled successfully'}), 200
+
+    data = request.get_json()
+    print("Received form data:", data)
+
+    new_order = {
+        'name': data['name'],
+        'phone': data['phone'],
+        'date_created': datetime.utcnow(),
+    }
+    orderslist.insert_one(new_order)
+    print(data)
+    return jsonify({'status': 'success', 'message': 'Form data saved successfully'}), 200
+
+def get_weighted_value():
+    values = [10, 20, 40]
+    probabilities = [0.5, 0.4, 0.1]
+    return random.choices(values, probabilities)[0]
+
+@app.route('/api/get_discount_value', methods=['GET'])
+def get_value():
+    value = get_weighted_value()
+    return jsonify({'value': value})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
