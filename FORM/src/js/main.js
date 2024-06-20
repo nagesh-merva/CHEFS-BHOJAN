bca40e4b561a6a0b2d3f90aa3eb4c85152b5ae2f
nagesh-merva <officialnagesh.merva@gmail.com>
2024-06-20 17:01:52 +0530

    modified

--- FORM/src/js/main.js
+++ FORM/src/js/main.js
@@ -14,7 +14,7 @@ document.getElementById('discountForm').addEventListener('submit', function (eve
         phone: phone
     };
 
-    fetch('https://chefs-bhojan-mlz6.vercel.app/api/save_form_data', {
+    fetch('http://localhost:8000/api/save_form_data', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -32,7 +32,7 @@ document.getElementById('discountForm').addEventListener('submit', function (eve
 });
 
 function fetchWeightedValue() {
-    fetch('https://chefs-bhojan-mlz6.vercel.app/api/get_discount_value')
+    fetch('http://localhost:8000/api/get_discount_value')
         .then(response => response.json())
         .then(data => {
             Discount = data.value
