2a88bac0706d9ab94a9da54e10c74f23920e82da
nagesh-merva <officialnagesh.merva@gmail.com>
2024-06-20 21:00:26 +0530

    modified

--- FORM_BACKEND/app.py
+++ FORM_BACKEND/app.py
@@ -7,12 +7,9 @@ import random
 
 app = Flask(__name__)
 
-CORS(app, supports_credentials=True, allow_headers="*", origins="*", methods=["OPTIONS", "POST"])
-CORS(app ,  resources={r"/api/*": {"origins": "*"}} ,supports_credentials=True)
+CORS(app ,allow_headers="*", resources={r"/api/*": {"origins": "https://chefs-bhojan.vercel.app"}} , supports_credentials=True)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'a_secure_default_key')
-
-
 
 client = MongoClient(
     'mongodb+srv://crob0008:GYfLnhxdJgeiOTPO@chefsbhojan.oxsu9gm.mongodb.net/',
@@ -37,11 +34,8 @@ def save_form_data():
         'date_created': datetime.utcnow(),
     }
     Deatils.insert_one(new_order)
-    response = jsonify({'status': 'success', 'message': 'Form data saved successfully'}), 200
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    response = jsonify({'message': 'Data saved successfully'})
+    return response
 
 def get_weighted_value():
     values = [10, 20, 40]
