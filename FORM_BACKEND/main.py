c430631949901b2adb82b0d0fce2e58e4d36e23b
nagesh-merva <officialnagesh.merva@gmail.com>
2024-06-20 17:19:34 +0530

    modified

--- FORM_BACKEND/main.py
+++ FORM_BACKEND/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'a_secure_default_key')
 
-CORS(app, resources={r"/api/*": {"origins": "https://chefs-bhojan.vercel.app"}})
+CORS(app,origins = "https://chefs-bhojan.vercel.app")
 
 client = MongoClient(
     'mongodb+srv://crob0008:GYfLnhxdJgeiOTPO@chefsbhojan.oxsu9gm.mongodb.net/',
